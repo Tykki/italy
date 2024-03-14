9b3a8f2569bd04694153202feb0dc68a66fb8883
Tykki <ayiri.etchie@gmail.com>
2024-03-14 08:47:33 -0500

    site finish?

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,6 +3,8 @@
 const pages = document.querySelectorAll('main')
 const navLinks = document.querySelectorAll('#navbar a')
 const gallery = document.getElementById("gallery")
+const infoList = document.querySelectorAll('.info-picker li')
+const infoText = document.querySelectorAll('.info-view p')
 
 const hide = () => {
     pages.forEach((page) => {
@@ -53,30 +55,27 @@ const hashHandler = (hash) => {
    })
  }
 
+ const infoHandler = (e) => {
+    infoText.forEach(text =>{
+        text.classList.add('hide')
+    })
+    if (e.id === 'ip1') {
+        infoText[0].classList.remove('hide')
+    } else if (e.id === 'ip2') {
+        infoText[1].classList.remove('hide')
+    } else if (e.id === 'ip3') {
+        infoText[2].classList.remove('hide')
+    } else if (e.id === 'ip4') {
+        infoText[3].classList.remove('hide')
+    }
+ }
+
+ infoList.forEach(item => {
+    item.addEventListener('click', e => infoHandler(e.target))
+})
+
  window.onmousemove = e => roam(e);
  window.ontouchmove = e => roam(e.touches[0])
-
-//  window.onmousemove = e => {
-//    const mouseX = e.clientX,
-//          mouseY = e.clientY;
-   
-//    const xDecimal = mouseX / window.innerWidth,
-//          yDecimal = mouseY / window.innerHeight;
-   
-//    const maxX = gallery.offsetWidth - window.innerWidth,
-//          maxY = gallery.offsetHeight - window.innerHeight;
-   
-//    const panX = maxX * xDecimal * -1,
-//          panY = maxY * yDecimal * -1;
-   
-//    gallery.animate({
-//      transform: `translate(${panX}px, ${panY}px)`
-//    }, {
-//      duration: 4000,
-//      fill: "forwards",
-//      easing: "ease"
-//    })
-//  }
 
  window.addEventListener('hashchange', (e) => hashHandler(e.target.location.hash))
  hashHandler(window.location.hash)
